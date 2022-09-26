ac8a91f59e5b7e10e4e6c902706042d8b15a3629
xeizer <keiser.form@gmail.com>
2022-09-26 09:11:59 +0700

    project 1

---
+++ app/Http/Livewire/Komen.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Komen extends Component
+{
+    public function render()
+    {
+        return view('livewire.komen');
+    }
+}
---
+++ resources/views/livewire/komen.blade.php
@@ -0,0 +1,26 @@
+<div>
+    @section('content')
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+
+                        <div class="card-body">
+                            Hei Netizen
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Nama Netizen</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                    placeholder="name@example.com">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Komentar Netizen</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+</div>
