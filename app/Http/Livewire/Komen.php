6219f9c13bb65e496684b9267b29f07f18bd6575
xeizer <keiser.form@gmail.com>
2022-09-26 11:21:24 +0700

    method simpan

--- app/Http/Livewire/Komen.php
+++ app/Http/Livewire/Komen.php
@@ -3,9 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Netizen;
 
 class Komen extends Component
 {
+    public $name;
+    public $komentar;
+    public function simpan()
+    {
+        $simpan = new Netizen();
+        $simpan->name = $this->name;
+        $simpan->komentar = $this->komentar;
+        $simpan->save();
+    }
+
     public function render()
     {
         return view('livewire.komen');
